deafd5312d4c18cf6e090ad48bc28cceec1d4aeb
jcmc0009 <jcmc0009@gmail.com>
2025-10-25 23:12:18 +0200

    Inicio

---
+++ src/adtarea1/ADtarea1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adtarea1;
+
+/**
+ *
+ * @author Jc
+ */
+public class ADtarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
